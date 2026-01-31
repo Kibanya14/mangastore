c2fa427ef68c5baa6e0660753e7817846b80525a
Kibanya14 <jonasmasamba14@gmail.com>
2026-01-31 09:39:44 +0100

    27

---
+++ migrations/versions/1069c02827c9_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 1069c02827c9
+Revises: 4d1f2a3b9c0e, e7c2f9a1b4d8
+Create Date: 2026-01-31 08:40:57.920686
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1069c02827c9'
+down_revision = ('4d1f2a3b9c0e', 'e7c2f9a1b4d8')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
